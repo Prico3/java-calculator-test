bfbc491bfe5d11ac92378e0fe518d2e5cd0ba80c
Prico3 <pricoco.andrea@gmail.com>
2023-03-24 15:39:43 +0100

    create test class

---
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -0,0 +1,24 @@
+package org.lessons.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void divide() {
+    }
+
+    @Test
+    void multiply() {
+    }
+}
