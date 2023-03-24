6748470c82841404d5b789f375d748928fb6b23b
Prico3 <pricoco.andrea@gmail.com>
2023-03-24 16:51:57 +0100

    fininsh

--- src/test/java/org/lessons/java/CalculatorTest.java
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -6,19 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+
+    private final Calculator calculator = new Calculator();
+
     @Test
-    void add() {
+    public void testAdd() {
+        assertEquals(4.0f, calculator.add(2.0f, 2.0f), 0.0001f);
+        assertEquals(4.0f, calculator.add(-2.0f, 6.0f), 0.0001f);
+
     }
 
     @Test
-    void subtract() {
+    public void testSubtract() {
+        assertEquals(3.0f, calculator.subtract(5.0f, 2.0f), 0.0001f);
     }
 
     @Test
-    void divide() {
+    public void testDivide() throws Exception {
+        assertEquals(2.0f, calculator.divide(10.0f, 5.0f), 0.0001f);
     }
 
     @Test
-    void multiply() {
+    public void testMultiply() {
+        assertEquals(24.0f, calculator.multiply(4.0f, 6.0f), 0.0001f);
     }
 }
